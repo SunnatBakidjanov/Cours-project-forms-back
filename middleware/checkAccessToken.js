1078ad1e5e5c8c4ec0d9d2a7d000fc8aeefbe604
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-06-28 17:16:36 +0500

    Добавил проверку юзера в checkAccessToken

--- middleware/checkAccessToken.js
+++ middleware/checkAccessToken.js
@@ -1,8 +1,23 @@
 const jwt = require('jsonwebtoken');
 const MESSAGES = require('../constants/messages');
+const { User } = require('../db/index');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
 	const authHeader = req.headers.authorization;
+
+	const user = await User.findOne({ where: email });
+
+	if (!user) {
+		return res.status(400).json({ message: MESSAGES.LOGIN.INCORRECT_LOGIN_OR_PASSWORD });
+	}
+
+	if (user.status === 'pending') {
+		return res.status(403).json({ message: MESSAGES.LOGIN.USER_STATUS_PENDING });
+	}
+
+	if (user.status === 'blocked') {
+		return res.status(403).json({ message: MESSAGES.LOGIN.BLOCKED_ACCAUNT });
+	}
 
 	if (!authHeader || !authHeader.startsWith('Bearer ')) {
 		return res.status(401).json({ message: MESSAGES.TOKEN.NO_TOKEN_PROVIDED });
