d34d0272d3130da44a014d769a9d4168b9a7d9c8
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-06-30 19:26:37 +0500

    Изменен createForm.js

--- service/form/createForm.js
+++ service/form/createForm.js
@@ -1,13 +1,9 @@
 const { Forms } = require('../../db/index');
 
 module.exports = async req => {
-	const { template_id } = req.body;
 	const user_id = req.user.id;
 
-	const newForm = await Forms.create({
-		template_id,
-		user_id,
-	});
+	const newForm = await Forms.create({ user_id });
 
 	return newForm;
 };
