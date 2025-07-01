4674126436ba07702e379e02717164f787348d5c
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-07-01 13:00:32 +0500

    Добавлен get запрос в форму
    -- Добавлен get запрос для получения формы из бд

--- controllers/formsController.js
+++ controllers/formsController.js
@@ -21,11 +21,11 @@ exports.updateForm = async (req, res) => {
 		const form = await Forms.findOne({ where: { key } });
 
 		if (!form) {
-			return res.status(404).json({ message: MESSAGES.FORM.NOT_FOUND });
+			return res.status(404).json({ message: MESSAGES.FORMS.NOT_FOUND });
 		}
 
 		if (form.user_id !== req.user.id) {
-			return res.status(403).json({ message: MESSAGES.FORBIDDEN });
+			return res.status(403).json({ message: MESSAGES.USER.USER_NOT_FOUND });
 		}
 
 		form.title = title;
@@ -36,6 +36,32 @@ exports.updateForm = async (req, res) => {
 		res.status(200).json({ message: MESSAGES.FORMS.SUCCESS });
 	} catch (err) {
 		console.error(err);
+		res.status(500).json({ message: MESSAGES.SERVER_ERROR });
+	}
+};
+
+const { Forms, User } = require('../db/index');
+const MESSAGES = require('../constants/messages');
+
+exports.getFormByKey = async (req, res) => {
+	try {
+		const { key } = req.params;
+		const form = await Forms.findOne({
+			where: { key },
+			include: [{ model: User, attributes: ['id', 'name', 'surname'] }],
+		});
+
+		if (!form) {
+			return res.status(404).json({ message: MESSAGES.FORMS.NOT_FOUND });
+		}
+
+		if (form.user_id !== req.user.id) {
+			return res.status(403).json({ message: MESSAGES.FORMS.NOT_FOUND });
+		}
+
+		res.json(form);
+	} catch (error) {
+		console.error(error);
 		res.status(500).json({ message: MESSAGES.SERVER_ERROR });
 	}
 };
