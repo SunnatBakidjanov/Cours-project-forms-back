d0e447426e6e2876e119a5ff31a8cca0ac2084ff
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-07-01 13:10:55 +0500

    Добавлен getformByKey в formsController

--- controllers/formsController.js
+++ controllers/formsController.js
@@ -39,3 +39,24 @@ exports.updateForm = async (req, res) => {
 		res.status(500).json({ message: MESSAGES.SERVER_ERROR });
 	}
 };
+
+exports.getFormByKey = async (req, res) => {
+	const { key } = req.params;
+
+	try {
+		const form = await Forms.findOne({ where: { key } });
+
+		if (!form) {
+			return res.status(404).json({ message: MESSAGES.FORMS.NOT_FOUND });
+		}
+
+		if (!form.isPublic && form.user_id !== req.user.id) {
+			return res.status(403).json({ message: MESSAGES.USER.USER_NOT_FOUND });
+		}
+
+		res.status(200).json(form);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: MESSAGES.SERVER_ERROR });
+	}
+};
